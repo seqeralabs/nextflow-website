99713f381250a61f28981ee87f6bd024d60fca34
Evan Floden <evanfloden@gmail.com>
2019-12-16 12:42:18 +0100

    Added dropdown menu and mobile for docs

--- themes/hugo-advance/assets/js/hamburger.js
+++ themes/hugo-advance/assets/js/hamburger.js
@@ -2,9 +2,16 @@ var body = document.querySelector('body');
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
 var hamburgerIcon = document.querySelector('.hamburger');
+var parentMenu = document.querySelector('li.hasChild > a');
 
 menuTrigger.onclick = function() {
   menuContainer.classList.toggle('open');
   hamburgerIcon.classList.toggle('is-active');
   body.classList.toggle('lock-scroll');
 };
+
+parentMenu.onclick = function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+	this.parentNode.classList.toggle('is-open');
+}
